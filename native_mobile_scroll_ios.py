72c9540d80e7a2e52b150c63efb553a323b1a6d2
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2024-04-14 22:54:55 -0700

    Update native_mobile_scroll_ios.py

--- native_mobile_scroll_ios.py
+++ native_mobile_scroll_ios.py
@@ -1,6 +1,7 @@
 import time
 from appium import webdriver
-from appium.webdriver.common.mobileby import MobileBy
+from appium.options.common import AppiumOptions
+from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -14,26 +15,32 @@ CAPS = {
         "app": "com.apple.Preferences"
     }
 }
-driver = webdriver.Remote(APPIUM, CAPS)
+OPTIONS = AppiumOptions().load_capabilities(CAPS)
+
+driver = webdriver.Remote(
+    command_executor=APPIUM,
+    options=OPTIONS
+)
+
 try:
     wait = WebDriverWait(driver, 3)
     siri_search = wait.until(EC.presence_of_element_located(
-        (MobileBy.ACCESSIBILITY_ID, "Siri & Search")))
+        (AppiumBy.ACCESSIBILITY_ID, "Siri & Search")))
     siri_search.click()
     wait.until(EC.presence_of_element_located(
-        (MobileBy.ACCESSIBILITY_ID, "ASK SIRI")))
+        (AppiumBy.ACCESSIBILITY_ID, "ASK SIRI")))
 
     # Scroll down and verify presence of the Shortcuts element at the bottom of the screen
     driver.execute_script("mobile: scroll", {"direction": "down"})
     wda = wait.until(EC.presence_of_element_located(
-        (MobileBy.ACCESSIBILITY_ID, "com.apple.shortcuts")))
+        (AppiumBy.ACCESSIBILITY_ID, "com.apple.shortcuts")))
     assert wda.text == "Shortcuts"
     time.sleep(3)   # for demo only
 
     # Scroll up and verify presence of the Siri Side Button element at the top of the screen
     driver.execute_script("mobile: scroll", {"direction": "up"})
     siri_side_button = wait.until(EC.presence_of_element_located(
-        (MobileBy.ACCESSIBILITY_ID, "ASSISTANT_HARDWARE_BUTTON_ID")))
+        (AppiumBy.ACCESSIBILITY_ID, "ASSISTANT_HARDWARE_BUTTON_ID")))
     assert siri_side_button.text == "Press Side Button for Siri"
     time.sleep(3)   # for demo only
 finally:
